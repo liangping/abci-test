f584d40951028dae90779574cf81c9bfd49c1d88
liangping <18786721@qq.com>
2022-03-22 14:16:21 +0800

    test abci

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+
+use std::net::{TcpStream, ToSocketAddrs};
+
+fn main() {
+    println!("Hello, world!");
+    abci();
+}
+
+fn abci() -> bool{
+
+    let cb = tendermint_abci::ClientBuilder::default();
+    let mut conn = cb.connect("localhost:26658").expect("Connected to server");
+    // let info = conn.?;
+    // println!("info: {:?}", info.last_block_height);
+    match conn.list_snapshots() {
+        Ok(snaps) => {
+            println!("connected.");
+            snaps.snapshots.iter().for_each(|x| {
+                println!("{:?}-{:?}", x.height, x.hash);
+            });
+        },
+        Err(e) => println!("Not connected.{}", e)
+    }
+    return true
+}
